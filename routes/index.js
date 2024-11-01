1a2c426b8b00f7792f6fa6d9336be92360a9f618
Antoine Correira <antoine.correira@gmail.com>
2024-11-01 12:55:24 +0100

    whatyearisit frontend

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/year', (req, res) => {
-  const currentYear = new Date;
-  res.json({date: String(currentYear.getFullYear())})
+  const currentYear = new Date().getFullYear();
+  res.json({year: currentYear.toString()})
 });
 
 module.exports = router;
